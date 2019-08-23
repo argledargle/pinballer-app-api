d332699aeeb322bafa3181ee360b724396c61d02
Alex Peter <apyells@gmail.com>
2019-08-23 11:29:22 -0500

    begins user-endpoints.spec.js

--- src/app.js
+++ src/app.js
@@ -4,14 +4,18 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config")
+const usersRouter = require('./users/users-router')
 
 const app = express();
 
 const morganOption = (NODE_ENV === 'production') ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-app.use(helmet());
+
 app.use(cors());
+app.use(helmet());
+
+app.use('/api/users', usersRouter)
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
