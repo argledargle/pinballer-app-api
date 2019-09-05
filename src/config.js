5c0db2485b89ff72cdd700da85f79f615b4146ba
Alex Peter <apyells@gmail.com>
2019-09-05 16:37:11 -0500

    updates for pushing to server

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL:
-    process.env.DB_URL || "postgresql://postgres:aRp3t3r12@localhost/pinballer",
+    process.env.DBATABASE_URL || "postgresql://postgres:aRp3t3r12@localhost/pinballer",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || 10800,
   CLIENT_ORIGIN: process.env.DB_URL || "postgresql://postgres:aRp3t3r12@localhost/pinballer-test"
