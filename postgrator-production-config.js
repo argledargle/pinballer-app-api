2907f6e55e4a17b836131ca01defd2defff9a65c
Alex Peter <apyells@gmail.com>
2019-09-06 10:18:42 -0500

    updates config files for server

--- postgrator-production-config.js
+++ postgrator-production-config.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
+
+//added validateChecksums: false to bypass checking of the checksums.
 
 module.exports = {
-  "migrationDirectory": "migrations",
-  "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
-}
+  migrationDirectory: "migrations",
+  driver: "pg",
+  host: process.env.MIGRATION_DB_HOST,
+  port: process.env.MIGRATION_DB_PORT,
+  database: process.env.MIGRATION_DB_NAME,
+  username: process.env.MIGRATION_DB_USER,
+  password: process.env.MIGRATION_DB_PASS,
+  validateChecksums: false
+};
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL:
-    process.env.DBATABASE_URL || "postgresql://postgres:aRp3t3r12@localhost/pinballer",
+    process.env.DBATABASE_URL || "postgres://upbtspbcmvjxsr:253a6a41a43c20a905c9a7ca4a1e29aec8bab8f2ce8b10afc8fcbbc27ae0e3ae@ec2-107-22-211-248.compute-1.amazonaws.com:5432/d7ciaupm2mg170",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || 10800,
   CLIENT_ORIGIN: process.env.DB_URL || "postgresql://postgres:aRp3t3r12@localhost/pinballer-test"
