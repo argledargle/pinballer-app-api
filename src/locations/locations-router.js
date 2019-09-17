53ac2bd6f696155c7ca428099d13fe6d97492b7d
Alex Peter <apyells@gmail.com>
2019-09-17 11:19:31 -0500

    changes req.query to req.params for /machines

--- src/locations/locations-router.js
+++ src/locations/locations-router.js
@@ -15,7 +15,7 @@ locationsRouter.route("/").get((req, res, next) => {
 
 //here, we get data for the machines at a specific location by location_id
 locationsRouter.route("/machines").get(jsonBodyParser, (req, res, next) => {
-  const { location_id } = req.query;
+  const { location_id } = req.params;
   if (!req.body)
     return res.status(400).json({
       error: `No location ID found.`
